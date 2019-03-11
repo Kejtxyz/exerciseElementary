9d70c671cffb722640d5aae59f1c4428bccd63ab
kasia <kgbsz@wp.pl>
2019-03-12 00:16:52 +0100

    zadanie3 podstawy

---
+++ src/exerciseElementary/exercise3/Main.java
@@ -0,0 +1,11 @@
+package exerciseElementary.exercise3;
+/**Przygotuj program, który wyświetli na konsoli liczby od 1 do 49. Użyj w tym celu pętli
+ for.*/
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i=1 ; i<=49 ;i++){
+            System.out.println(i);
+        }
+    }
+}
