45c44cbd6a41ab81255293c7c4c706c53060f16f
kasia <kgbsz@wp.pl>
2019-03-12 00:20:07 +0100

    zadanie4 podstawy

---
+++ src/exerciseElementary/exercise4/Main.java
@@ -0,0 +1,12 @@
+package exerciseElementary.exercise4;
+/**Przygotuj program, który wyświetli na konsoli liczby od 1 do 49. Użyj w tym celu pętli
+ while*/
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <= 49){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
