b2a82e12083ddb7189007600f0c77006494c754a
kasia <kgbsz@wp.pl>
2019-03-14 19:06:54 +0100

    zadanie 9 - przelicza C na F

--- src/exerciseElementary/Main.java
+++ src/exerciseElementary/Main.java
@@ -1,4 +1,23 @@
 package exerciseElementary;
 
+import java.util.Scanner;
+
+/**Przygotuj program, który pobierze od użytkownika długość podstawy trójkąta oraz jego
+ wysokość, a następnie obliczy jego pole powierzchni*/
 public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        System.out.println("Podaj dlugosc podstawy trojkata");
+        Scanner dlugosc = new Scanner(System.in);
+        int c =  dlugosc.nextInt();
+
+        System.out.println("Podaj wysokosc trojkat");
+        Scanner wysokosc = new Scanner(System.in);
+        int d = wysokosc.nextInt();
+
+        int e = (c*d) /2;
+        System.out.println(e);
+
+    }
 }
---
+++ src/exerciseElementary/exercise9.java
@@ -0,0 +1,15 @@
+package exerciseElementary;
+
+import java.util.Scanner;
+
+/**Przygotuj program, który pobierze od użytkownika temperaturę w o C a następnie
+ zamieni ją na temperaturę w o F*/
+public class exercise9 {
+    public static void main(String[] args) {
+        System.out.println("Podaj temperature w stopniach cencjusza ");
+        Scanner tempC = new Scanner(System.in);
+        double c = tempC.nextDouble();
+        c = c*9/5+32;
+        System.out.println(c);
+    }
+}
